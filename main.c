aa5932e04baab28e8607bd842281482c9878b2a4
Santi Rodriguez-Parera <santi.twn@gmail.com>
2024-07-20 15:54:12 +0000

    Malloc

--- main.c
+++ main.c
@@ -14,7 +14,8 @@
 #include <stdlib.h>
 
 
-int ft_len(const char *str) {
+int ft_len(const char *str)
+{
     int n = 0;
     while (*str) {
         n++;
@@ -26,28 +27,33 @@ int ft_len(const char *str) {
 
 char *str_zeros(const char *str, int n) 
 {
-    char *new_str = malloc(n + 1); 
-   
+    char *new_str;
+    int i = 0;
 
+    new_str = malloc(n + 1); 
+    
+    
 
-
-    for (int i = 0; i < n; i++) {
+    while (i < n) 
+    {
         new_str[i] = '0'; 
+        i++;
     }
     new_str[n] = '\0'; 
 
     return new_str;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
     if (argc != 2) {
+        
         fprintf(stderr, "Uso: %s <cadena>\n", argv[0]);
         return 1;
     }
 
     int n = ft_len(argv[1]);
     char *copy = str_zeros(argv[1], n); 
-
     printf("El número es: %s\n", argv[1]);
     printf("El copy es: %s\n", copy);
     printf("El array tiene: %d dígitos\n", n);
@@ -56,3 +62,4 @@ int main(int argc, char **argv) {
 
     return 0;
 }
+
